5d63aaecf54057ccabd9993981d30f32778a3186
M Rahman <ctgeye.news@gmail.com>
2025-05-06 17:09:13 +0600

    Contacts Elements

--- resources/views/frontend/include/contacts/contact-details.blade.php
+++ resources/views/frontend/include/contacts/contact-details.blade.php
@@ -2,7 +2,7 @@
     <div class="subtitle wow fadeInUp mb-3">Contact Form</div>
     <h2 class="wow fadeInUp">Get In Touch</h2>
 
-    <p>Whether you have a question, a suggestion, or just want to say hello, this is the place to do it. Please fill out the form below with your details and message, and we'll get back to you as soon as possible.</p>
+    <p>Looking to transform your space or inquire about our interior design services? Our team at RTR Interior is here to help. Whether you're planning a residential renovation, a commercial fit-out, or simply have questions about our design process, we’d love to hear from you. Visit our office at Dragon Mart-1, International City, Al Awir Road – Dubai, U.A.E, or reach out through the contact form. We’re committed to delivering creative, functional, and elegant interiors that reflect your vision.</p>
 
     <div class="spacer-single"></div>
 
@@ -18,18 +18,27 @@
         </div>
 
         <div class="col-lg-6">
-            <div class="fw-bold text-dark"><i class="icofont-location-pin me-2 id-color-2"></i>Office Location</div>
-            100 S Main St, Los Angeles, CA
+            <div class="fw-bold text-dark">
+                <i class="icofont-location-pin me-2 id-color-2"></i>
+                Office Location
+            </div>
+            Dragon Mart-1, International City, Al Awir Road - Dubai - U.A.E
 
             <div class="spacer-single"></div>
 
-            <div class="fw-bold text-dark"><i class="icofont-envelope me-2 id-color-2"></i>Send a Message</div>
-            contact@mindthera.com
+            <div class="fw-bold text-dark">
+                <i class="icofont-envelope me-2 id-color-2"></i>
+                Send a Message
+            </div>
+            contact@rtrinterior.com
 
             <div class="spacer-single"></div>
 
-            <div class="fw-bold text-dark"><i class="icofont-phone me-2 id-color-2"></i>Call Us Directly</div>
-            +929 333 9296
+            <div class="fw-bold text-dark">
+                <i class="icofont-phone me-2 id-color-2"></i>
+                Call Us Directly
+            </div>
+                058 681 3322
         </div>
     </div>
 
