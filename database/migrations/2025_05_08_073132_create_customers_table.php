22d815956bec0dd3508b7749138e0fc59a0c6954
M Rahman <ctgeye.news@gmail.com>
2025-05-10 21:03:52 +0600

    customers table

--- database/migrations/2025_05_08_073132_create_customers_table.php
+++ database/migrations/2025_05_08_073132_create_customers_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->text('address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
